41b6c256189ca90133d603fd43835cdabe675ecd
hemagunnala <gunnalahema14134@gmail.com>
2023-07-30 00:19:36 +0530

    Final commit

--- src/pages/Notes/panels/Editor/index.js
+++ src/pages/Notes/panels/Editor/index.js
@@ -3,15 +3,11 @@ import Editor from '@draft-js-plugins/editor';
 import { EditorState } from 'draft-js';
 import createInlineToolbarPlugin from '@draft-js-plugins/inline-toolbar';
 import createSideToolbarPlugin from '@draft-js-plugins/side-toolbar';
-import { MdAddCircleOutline } from 'react-icons/md'; // Import the add icon from a library like react-icons
 
-import './editor.module.scss';
 import 'draft-js/dist/Draft.css';
 import '@draft-js-plugins/inline-toolbar/lib/plugin.css';
 import '@draft-js-plugins/side-toolbar/lib/plugin.css';
 
-// Creates an Instance. At this step, a configuration object can be passed in
-// as an argument.
 const sideToolbarPlugin = createSideToolbarPlugin();
 const inlineToolbarPlugin = createInlineToolbarPlugin();
 
@@ -26,14 +22,9 @@ const MyEditor = () => {
   const changeState = (state) => {
     setEditorState(state);
   };
-  const [addItem, setAddItem] = useState(null);
-
-  const addAssignment = () => {
-    setAddItem('assignment');
-  };
 
   return (
-    <div className="App">
+    <div>
       <Editor
         editorState={editorState}
         onChange={changeState}
@@ -42,72 +33,8 @@ const MyEditor = () => {
       />
       <InlineToolbar />
       <SideToolbar />
-      <div >
-      <MdAddCircleOutline onClick={addAssignment} />
-      </div>
-
-    {addItem === 'assignment' && (
-      <div>
-        {/* Render your assignment widget form or component here */}
-        <button onClick={() => setAddItem(null)}></button>
-      </div>
-    )}
     </div>
   );
 };
 
 export default MyEditor;
-
-// import React, { useEffect, useMemo, useRef, useState } from 'react';
-// // import { EditorState } from 'draft-js';
-// import Editor, { createEditorStateWithText } from '@draft-js-plugins/editor';
-// import createInlineToolbarPlugin from '@draft-js-plugins/inline-toolbar';
-// import styles from './editor.module.scss';
-// // import 'draft-js/dist/Draft.css';
-// // import '@draft-js-plugins/inline-toolbar/lib/plugin.css';
-
-// const text =
-//   'In this editor a toolbar shows up once you select part of the text …';
-
-// const SimpleInlineToolbarEditor = () => {
-//   const [plugins, InlineToolbar] = useMemo(() => {
-//     const inlineToolbarPlugin = createInlineToolbarPlugin();
-//     return [[inlineToolbarPlugin], inlineToolbarPlugin.InlineToolbar];
-//   }, []);
-
-//   const [editorState, setEditorState] = useState(() =>
-//     createEditorStateWithText('')
-//   );
-
-//   useEffect(() => {
-//     // fixing issue with SSR https://github.com/facebook/draft-js/issues/2332#issuecomment-761573306
-//     setEditorState(createEditorStateWithText(text));
-//   }, []);
-
-//   const editor = useRef(null);
-
-//   const onChange = (value) => {
-//     setEditorState(value);
-//   };
-
-//   const focus = () => {
-//     editor.current?.focus();
-//   };
-
-//   return (
-//     <div className={styles.editor} onClick={focus}>
-//       <Editor
-//         editorKey="SimpleInlineToolbarEditor"
-//         editorState={editorState}
-//         onChange={onChange}
-//         plugins={plugins}
-//         ref={(element) => {
-//           editor.current = element;
-//         }}
-//       />
-//       <InlineToolbar />
-//     </div>
-//   );
-// };
-
-// export default SimpleInlineToolbarEditor;
